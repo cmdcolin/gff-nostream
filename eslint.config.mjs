a625e9066512497c480cfce4f523b4650518820c
Colin <colin.diesh@gmail.com>
2025-10-12 09:38:10 -0400

    Updates

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,9 @@
 import eslint from '@eslint/js'
+import { defineConfig } from 'eslint/config'
+import importPlugin from 'eslint-plugin-import'
 import tseslint from 'typescript-eslint'
 
-export default tseslint.config(
+export default defineConfig(
   {
     ignores: ['esm/**/*', 'dist/**/*', '*.js', '*.mjs', 'example/*'],
   },
@@ -17,6 +19,7 @@ export default tseslint.config(
   ...tseslint.configs.recommended,
   ...tseslint.configs.stylisticTypeChecked,
   ...tseslint.configs.strictTypeChecked,
+  importPlugin.flatConfigs.recommended,
   {
     rules: {
       '@typescript-eslint/no-unused-vars': [
@@ -44,48 +47,23 @@ export default tseslint.config(
       '@typescript-eslint/prefer-nullish-coalescing': 'off',
       '@typescript-eslint/require-await': 'off',
       '@typescript-eslint/no-dynamic-delete': 'off',
-
-      'unicorn/no-new-array': 'off',
-      'unicorn/no-empty-file': 'off',
-      'unicorn/prefer-type-error': 'off',
-      'unicorn/prefer-modern-math-apis': 'off',
-      'unicorn/prefer-node-protocol': 'off',
-      'unicorn/no-unreadable-array-destructuring': 'off',
-      'unicorn/no-abusive-eslint-disable': 'off',
-      'unicorn/no-array-callback-reference': 'off',
-      'unicorn/number-literal-case': 'off',
-      'unicorn/prefer-add-event-listener': 'off',
-      'unicorn/prefer-top-level-await': 'off',
-      'unicorn/consistent-function-scoping': 'off',
-      'unicorn/no-await-expression-member': 'off',
-      'unicorn/no-lonely-if': 'off',
-      'unicorn/consistent-destructuring': 'off',
-      'unicorn/prefer-module': 'off',
-      'unicorn/prefer-optional-catch-binding': 'off',
-      'unicorn/no-useless-undefined': 'off',
-      'unicorn/no-null': 'off',
-      'unicorn/no-nested-ternary': 'off',
-      'unicorn/filename-case': 'off',
-      'unicorn/catch-error-name': 'off',
-      'unicorn/prevent-abbreviations': 'off',
-      'unicorn/prefer-code-point': 'off',
-      'unicorn/numeric-separators-style': 'off',
-      'unicorn/no-array-for-each': 'off',
-      'unicorn/prefer-spread': 'off',
-      'unicorn/explicit-length-check': 'off',
-      'unicorn/prefer-regexp-test': 'off',
-      'unicorn/relative-url-style': 'off',
-      'unicorn/prefer-math-trunc': 'off',
-      'unicorn/prefer-query-selector': 'off',
-      'unicorn/no-negated-condition': 'off',
-      'unicorn/switch-case-braces': 'off',
-      'unicorn/prefer-switch': 'off',
-      'unicorn/better-regex': 'off',
-      'unicorn/no-for-loop': 'off',
-      'unicorn/escape-case': 'off',
-      'unicorn/prefer-number-properties': 'off',
-      'unicorn/no-process-exit': 'off',
-      'unicorn/prefer-at': 'off',
+      'import/no-unresolved': 'off',
+      'import/order': [
+        'error',
+        {
+          named: true,
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+          },
+          groups: [
+            'builtin',
+            ['external', 'internal'],
+            ['parent', 'sibling', 'index', 'object'],
+            'type',
+          ],
+        },
+      ],
     },
   },
 )
